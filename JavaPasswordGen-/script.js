18997f1588ed47bcc431fe1cb5fb02e7303b614d
WeWantSomeMo <104116721+WeWantSomeMo@users.noreply.github.com>
2022-09-25 17:42:49 -0500

    Add files via upload

---
+++ JavaPasswordGen-/script.js
@@ -0,0 +1,44 @@
+// Assignment Code
+const alpha = "abcdefghijklmnopqestuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*_=+";
+const passwordTxt = document.getElementById("password")
+const length = document.getElementById("length")
+const incNumbers = document.getElementById("numbers");
+const incSymbols = document.getElementById("symbols");
+
+
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordTxt = document.querySelector("password");
+
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", () => {
+  let characters = alpha;
+  incNumbers.checked ? (characters += numbers) : "";
+  incSymbols.checked ? (characters += symbols) : "";
+  passwordTxt.value = generatePassword(length.value, characters);
+});
+
+const generatePassword = (length, characters => {
+  let password = "";
+  for (let i = 0; i <length; i++) {
+    password += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  return password;
+});
+
+const copyBtn = document.getElementById("copy");
+copyBtn.addEventListener("click", () => {
+  passwordTxt.select();
+  document.execCommand("copy");
+  alert("Password Copied");
+});}
